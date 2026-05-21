5d196ca352102c514205a862bde0108cd0f76707
ygglue <lagumbayeliyahu@gmail.com>
2026-05-21 18:18:10 +0800

    feat: /contact route page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import ContactSection from "@/components/ContactSection";
+
+export default function ContactPage() {
+  return (
+    <div className="flex flex-col min-h-full p-6 md:p-12">
+      <div className="max-w-4xl">
+        <ContactSection />
+      </div>
+    </div>
+  );
+}
